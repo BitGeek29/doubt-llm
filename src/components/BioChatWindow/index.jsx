77a73e18f8c96693ea1c39b8370afd5af5f50496
Haseeb Shaikh <haseeb.shaikh@ggcollege.in>
2025-03-01 14:26:51 +0000

    fixes

--- src/components/BioChatWindow/index.jsx
+++ src/components/BioChatWindow/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MarkdownPreviewer from '../MarkdownPreviewer';
 
 const getAccessToken = async () => {
